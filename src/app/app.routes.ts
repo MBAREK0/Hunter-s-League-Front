8c32154a39e73bc5620e481312c941fc56f32246
MBAREK0 <elaadraouimbarek2023@gmail.com>
2024-12-30 23:10:34 +0100

    added the project structure and create the login-register and layout  ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../features/auth/login/login.component';
+import { RegisterComponent } from '../features/auth/register/register.component';
+import { DashboardComponent} from "../features/dashboard/dashboard.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
